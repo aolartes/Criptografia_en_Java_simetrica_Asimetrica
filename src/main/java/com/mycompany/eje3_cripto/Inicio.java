6db107e0b62cfe65b1ea21401ee6e291312bd85c
aolartes <alvaroolartes@hotmail.com>
2023-03-20 18:52:11 -0500

    Finish Asimetrico-Simetrico

--- src/main/java/com/mycompany/eje3_cripto/Inicio.java
+++ src/main/java/com/mycompany/eje3_cripto/Inicio.java
@@ -11,10 +11,11 @@ package com.mycompany.eje3_cripto;
 public class Inicio  {
 
     public static void main(String[] args) {
+        FrameIntro ObjetoIntro = new FrameIntro();
+        ObjetoIntro.setVisible(true);
         
-        VistaUsers objetoVista = new VistaUsers();
-        objetoVista.setVisible(true);
-        
+        //VistaUsers objetoVista = new VistaUsers();
+        //objetoVista.setVisible(true);       
         
     }
 }
